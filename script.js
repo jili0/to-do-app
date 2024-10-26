ff69528e7e55df7ccd03356a8fb95e6170dc90dd
Jing <jingli0319@gmail.com>
2024-10-26 23:21:13 +0200

    improve structure

--- script.js
+++ script.js
@@ -8,31 +8,30 @@ const renderTasks = () => {
   taskContainer.innerHTML = "";
   if (tasks.length) {
     tasks.forEach((task) => {
-    const taskHtml = `
+      const taskHtml = `
     <div class="taskItem" id="task-${task.taskID}">
       <p id="text-${task.taskID}" style=${
-      task.isDone ? "text-decoration:line-through;" : "text-decoration:none;"
-    }>${task.task}</p>
+        task.isDone ? "text-decoration:line-through;" : "text-decoration:none;"
+      }>${task.task}</p>
       <div class="controlBtns">
         <button id="check-${
           task.taskID
         }"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="${
-      task.isDone
-        ? "M280-200v-80h284q63 0 109.5-40T720-420q0-60-46.5-100T564-560H312l104 104-56 56-200-200 200-200 56 56-104 104h252q97 0 166.5 63T800-420q0 94-69.5 157T564-200H280Z"
-        : "M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"
-    }"></path></svg></button>
+        task.isDone
+          ? "M280-200v-80h284q63 0 109.5-40T720-420q0-60-46.5-100T564-560H312l104 104-56 56-200-200 200-200 56 56-104 104h252q97 0 166.5 63T800-420q0 94-69.5 157T564-200H280Z"
+          : "M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"
+      }"></path></svg></button>
         <button id="delete-${
           task.taskID
         }"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg></button>
       </div>
     </div>
     `;
-    taskContainer.innerHTML += taskHtml;
-  });  
+      taskContainer.innerHTML += taskHtml;
+    });
   } else {
     taskContainer.textContent = "You don't have any task now.";
   }
-
 };
 
 renderTasks();
@@ -45,6 +44,7 @@ const createTask = () => {
     taskInputField.value = "";
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }
+  renderTasks();
 };
 
 const checkTask = (e) => {
@@ -70,7 +70,6 @@ const handleClick = (e) => {
   if (!e.target.closest("button")) return;
   if (e.target.id === "createTask") {
     createTask();
-    renderTasks();
   } else if (e.target.closest("button").id.includes("check")) {
     checkTask(e);
   } else if (e.target.closest("button").id.includes("delete")) {
@@ -79,3 +78,4 @@ const handleClick = (e) => {
 };
 
 document.body.addEventListener("click", handleClick);
+ 
